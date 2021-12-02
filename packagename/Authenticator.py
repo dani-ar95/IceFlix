a208a431453b377fad86e83907c98fe7839356d1
Álvaro Fernández Santos <alvarofernandez24@gmail.com>
2021-12-02 09:38:23 +0100

    ADD Authenticator

--- packagename/Authenticator.py
+++ packagename/Authenticator.py
@@ -1,45 +1,89 @@
 import sys, Ice
 import IceFlix
+import json
+import secrets
+
+from packagename.Iceflix_ice import Unauthorized
 
 class AuthenticatorI(IceFlix.Authenticator):
 
     def refreshAuthorization(self, user, passwordHash, current=None):
         # Código
+        obj = json.load(open("users.json"))
+
+        for i in xrange(len(obj)):
+            if obj[i]["user"] == user and obj[i]["password"] == passwordHash:
+                new_token = secrets.token_urlsafe(40)
+                obj[i]["user_token"] = new_token
+                return new_token
+   
+        raise Unauthorized
         # Throws Unauthorized
         # Retorna String
         pass
 
     def isAuthorized(self,userToken, current=None):
         # Código
+        obj = json.load(open("users.json"))
+
+        for i in xrange(len(obj)):
+            if obj[i]["user_token"] == userToken:
+                return True
+
+        raise Unauthorized
         # Retorna boolean
         pass
 
     def whois(self, userToken, current=None):
         # Código
+        obj = json.load(open("users.json"))
+
+        for i in xrange(len(obj)):
+            if obj[i]["user_token"] == userToken:
+                return obj[i]["user"]
+
+        raise Unauthorized      
         # Throws Unauthorized
         # Retorna string
         pass
 
     def addUser(self, user, passwordHash, adminToken, current=None):
-        # Código
+        # Comprobar admin
+
+        # raise Unauthorized
+        # Añadir usuario
+        with open ("users.json", "r+") as fp:
+            data = json.load(fp)
+            data["user"] = user
+            data["password"] = passwordHash
+            data["user_token"] = secrets.token_urlsafe(40)
+            fp.seek(0)
+            json.dump(data, fp, indent=4)
+            fp.truncate()
         # Throws Unauthorized
-        pass
 
     def removeUser(self, user, adminToken, current=None):
-        # Código
+        # Comprobar admin
+
+        # raise Unautorized
+        # Eliminar usuario
+        obj = json.load(open("users.json"))
+
+        for i in xrange(len(obj)):
+            if obj[i]["user"] == user:
+                obj.pop(i)
+                break
         # Throws Unauthorized
-        pass
 
 if __name__ == "__main__":
+    with Ice.initialize(sys.argv) as communicator:
+        base = communicator.stringToProxy("MainID:default -p 10000")
+        controller = IceFlix.MainPrx.checkedCast(base)
+        o = AuthenticatorI()
+        if not controller:
+            raise RuntimeError("Invalid proxy")
 
-   with Ice.initialize(sys.argv) as communicator:
-    base = communicator.stringToProxy("MainID:default -p 10000")
-    controller = IceFlix.MainPrx.checkedCast(base)
-    o = AuthenticatorI()
-    if not controller:
-        raise RuntimeError("Invalid proxy")
-
-    controller.register(o)
+        controller.register(o)
 
 #with Ice.initialize(sys.argv) as communicator:
 ##    adapter = communicator.createObjectAdapterWithEndpoints("Authenticator", "default -p 10000")
