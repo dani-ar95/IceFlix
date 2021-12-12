181ff9ba796c446373536dd89e4ef2970848c771
Álvaro Fernández Santos <alvarofernandez24@gmail.com>
2021-12-12 13:46:44 +0100

    mediaUploader organizado

--- IceFlix/MediaUploader.py
+++ IceFlix/MediaUploader.py
@@ -1,46 +1,56 @@
 #!/usr/bin/python3
+# pylint: disable=invalid-name
+
+''' Clase contenedora de archivos para poder subirlos al servidor '''
 
 import sys
 from os import path
 import Ice
+import IceFlix
 
 SLICE_PATH = path.join(path.dirname(__file__), "iceflix.ice")
 Ice.loadSlice(SLICE_PATH)
-import IceFlix
 
 
-class MediaUploaderI(IceFlix.MediaUploader):
+class MediaUploaderI(IceFlix.MediaUploader): # pylint: disable=inherit-non-class
+    ''' Insntancia de MediaUploader'''
 
     def __init__(self, file_name):
         try:
-            self._fd_ = open(file_name, "rb") # pylint: disable=bad-option-value
+            self._fd_ = open(file_name, "rb") # pylint: disable=bad-option-value,consider-using-with
         except FileNotFoundError:
             self._fd_ = None
             print("Archivo no encontrado: " + str(file_name))
 
     def receive(self, size: int, current=None): # pylint: disable=unused-argument
+        ''' Retorna la cantidad indicada del fichero que contiene '''
+
         if self._fd_:
             chunk = self._fd_.read(size)
             return chunk
 
     def close(self, current=None): # pylint: disable=unused-argument
+        ''' Cierra el descriptor del archivo que contiene '''
+
         if self._fd_:
             self._fd_.close()
 
 
 class MediaUploaderServer(Ice.Application):
-    def run(self, argv):
+    ''' Servidor de Media Uploader '''
+
+    def run(self, args):
         #sleep(1)
         self.shutdownOnInterrupt()
-        main_service_proxy = self.communicator().stringToProxy(argv[1])
+        main_service_proxy = self.communicator().stringToProxy(args[1])
         main_connection = IceFlix.MainPrx.checkedCast(main_service_proxy)
         if not main_connection:
             raise RuntimeError("Invalid proxy")
 
         broker = self.communicator()
-        
+
         self.shutdownOnInterrupt()
         broker.waitForShutdown()
-        
+
 if __name__ == "__main__":
     sys.exit(MediaUploaderServer().main(sys.argv))
