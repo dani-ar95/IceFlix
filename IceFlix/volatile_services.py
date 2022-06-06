c958098777e748e565d3266ec13722b683bb3c76
Sergio Garrido <58768971+sergiogm8@users.noreply.github.com>
2022-06-06 16:12:26 +0200

    Clase VolatileServices

---
+++ IceFlix/volatile_services.py
@@ -0,0 +1,12 @@
+import Ice # pylint: disable=import-error,wrong-import-position
+from os import path
+
+SLICE_PATH = path.join(path.dirname(__file__), "iceflix.ice")
+Ice.loadSlice(SLICE_PATH)
+import IceFlix # pylint: disable=import-error,wrong-import-position
+
+class VolatileServices(IceFlix.VolatileServices):
+    def __init__(self, auth_services, catalog_services):
+        '''Inicializa el objeto.'''
+        self.authenticators = auth_services
+        self.mediaCatalogs = catalog_services # pylint: disable=invalid-name 
