13775aba531499aedc7c3b4c4240729528129f91
AlvaroFernandezSantos <alvarofernandez24@gmail.com>
2022-06-06 14:11:36 +0200

    Cambios en main

--- IceFlix/Main.py
+++ IceFlix/Main.py
@@ -5,11 +5,17 @@
 from os import path
 import sys
 import Ice
+import uuid
+import IceStorm
+from iceevents import IceEvents
+from service_announcement import ServiceAnnouncementsListener, ServiceAnnouncementsSender
 
 SLICE_PATH = path.join(path.dirname(__file__), "iceflix.ice")
 
 Ice.loadSlice(SLICE_PATH)
 import IceFlix
+
+from constants import ANNOUNCEMENT_TOPIC, ICESTORM_PROXY_PROPERTY
 
 
 class MainI(IceFlix.Main): # pylint: disable=inherit-non-class
@@ -61,8 +67,56 @@ class MainI(IceFlix.Main): # pylint: disable=inherit-non-class
 
         return adminToken == self._token_ # pylint: disable=invalid-name
 
+    def share_data_with(self, service):
+        """Share the current database with an incoming service."""
+        service.updateDB(None, self.service_id)
+
+    def updateDB(
+        self, values, service_id, current
+    ):  # pylint: disable=invalid-name,unused-argument
+        """Receives the current main service database from a peer."""
+        print(
+            "Receiving remote data base from %s to %s", service_id, self.service_id
+        )
+
+
 class MainServer(Ice.Application):
     """Servidor del servicio principal"""
+
+    def __init__(self):
+        super().__init__()
+        self.servant = MainI()
+        self.proxy = None
+        self.adapter = None
+        self.announcer = None
+        self.subscriber = None
+
+    def setup_announcements(self):
+        """Configure the announcements sender and listener."""
+
+        communicator = self.communicator()
+        topic_manager = IceStorm.TopicManagerPrx.checkedCast(
+            communicator.propertyToProxy("IceStorm.TopicManager")
+        )
+
+        try:
+            topic = topic_manager.create("ServiceAnnouncements")
+        except IceStorm.TopicExists:
+            topic = topic_manager.retrieve("ServiceAnnouncements")
+
+        self.announcer = ServiceAnnouncementsSender(
+            topic,
+            self.servant.service_id,
+            self.proxy,
+        )
+
+        self.subscriber = ServiceAnnouncementsListener(
+            self.servant, self.servant.service_id, IceFlix.MainPrx
+        )
+
+        subscriber_prx = self.adapter.addWithUUID(self.subscriber)
+        topic.subscribeAndGetPublisher({}, subscriber_prx)
+
 
     def run(self, argv):
         ''' Implementación del servidor principal '''
@@ -71,13 +125,18 @@ class MainServer(Ice.Application):
         properties = broker.getProperties()
         servant._token_ = properties.getProperty("AdminToken")
 
-        adapter = broker.createObjectAdapter("MainAdapter")
-        adapter.add(servant, broker.stringToIdentity("Main"))
+        self.adapter = broker.createObjectAdapter(self.subscriber)
+        self.adapter.add(servant, broker.stringToIdentity("Main"))
 
-        adapter.activate()
+        self.adapter.activate()
+
+        self.setup_announcements()
+        self.announcer.start_service()
 
         self.shutdownOnInterrupt()
         broker.waitForShutdown()
+
+        self.announcer.stop()
 
         return 0
 
