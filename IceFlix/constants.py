d58141257b5f40bc93ec52736c9f99e64ecd585f
AlvaroFernandezSantos <alvarofernandez24@gmail.com>
2022-06-06 10:29:16 +0200

    Plantilla de IceStorm

---
+++ IceFlix/constants.py
@@ -0,0 +1,7 @@
+ANNOUNCEMENT_TOPIC = 'ServiceAnnouncements'
+REVOCATIONS_TOPIC = 'Revocations'
+AUTH_SYNC_TOPIC = 'UserUpdates'
+CATALOG_SYNC_TOPIC = 'CatalogUpdates'
+STREAM_ANNOUNCES_TOPIC = 'StreamAnnouncements'
+
+ICESTORM_PROXY_PROPERTY = 'IceStorm.TopicManager.Proxy'
