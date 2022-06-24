479cb9a2aaa3295ba3c266d39d6f730e4808abf2
AlvaroFernandezSantos <alvarofernandez24@gmail.com>
2022-06-24 19:01:24 +0200

    Limpiado main

--- IceFlix/Main.py
+++ IceFlix/Main.py
@@ -4,26 +4,30 @@
 
 from os import path
 import sys
-import Ice
-import uuid
-import IceStorm
 import random
-from volatile_services import VolatileServices
-from service_announcement import ServiceAnnouncementsListener, ServiceAnnouncementsSender
-from register_services import RegisterServices
+import uuid
+import Ice
+from IceStorm import TopicManagerPrx, TopicExists # pylint: disable=no-name-in-module
+from volatile_services import VolatileServices # pylint: disable=no-name-in-module, import-error
+from service_announcement import ServiceAnnouncementsListener, ServiceAnnouncementsSender # pylint: disable=no-name-in-module, import-error
+from constants import ANNOUNCEMENT_TOPIC # pylint: disable=no-name-in-module
 
 SLICE_PATH = path.join(path.dirname(__file__), "iceflix.ice")
 
 Ice.loadSlice(SLICE_PATH)
-import IceFlix
+try:
+    import IceFlix
+except ImportError:
+    Ice.loadSlice(path.join(path.dirname(__file__), "iceflix.ice"))
+    import IceFlix # pylint: disable=wrong-import-position
 
-from constants import ANNOUNCEMENT_TOPIC, ICESTORM_PROXY_PROPERTY
 
 
 class MainI(IceFlix.Main): # pylint: disable=inherit-non-class
     """Sirviente del servicio principal"""
 
     def __init__(self):
+        self._token_ = None
         self._servants_ = set()
         self.service_id = str(uuid.uuid4())
         self.auth_services = []
@@ -33,45 +37,71 @@ class MainI(IceFlix.Main): # pylint: disable=inherit-non-class
 
     @property
     def get_volatile_services(self):
+        """ Devuelve un objeto VolatlieServices """
+
         return VolatileServices(self.auth_services, self.catalog_services)
 
 
-    def getAuthenticator(self, current=None):
+    def getAuthenticator(self, current=None): # pylint: disable=invalid-name,unused-argument
+        """ Devuelve un authenticator activo """
+
         while self.auth_services:
-            print("Iterando en authenticators")
             try:
                 auth_prx = random.choice(list(self.auth_services))
                 auth_prx.ice_ping()
-                print('\n[MAIN] Se ha encontrado el proxy de autenticador: ', auth_prx)
                 return IceFlix.AuthenticatorPrx.uncheckedCast(auth_prx)
-            
+
             except Ice.ConnectionRefusedException:
                 self.auth_services.remove(auth_prx)
 
-        print("\n[MAIN] No hay servicios de autenticación disponibles")
+        print(f"\n[MAIN] ID: {self.service_id} No hay servicios de autenticación disponibles")
         raise IceFlix.TemporaryUnavailable
-    
-    def addAuthenticator(self, auth_prx, current=None):
+
+    def addAuthenticator(self, auth_prx, current=None): # pylint: disable=invalid-name,unused-argument
+        """ Añade un authenticator activo a su lista """
+
         self.auth_services.append(auth_prx)
 
 
-    def getCatalog(self, current=None):
+    def getCatalog(self, current=None): # pylint: disable=invalid-name,unused-argument
+        """ Devuelve un MediaCatalog activo """
+
         while self.catalog_services:
             try:
                 catalog_prx = random.choice(list(self.catalog_services))
                 catalog_prx.ice_ping()
-                print('\n[MAIN] Se ha encontrado el proxy de catálogo: ', catalog_prx)
                 return IceFlix.MediaCatalogPrx.uncheckedCast(catalog_prx)
-            
+
             except Ice.ConnectionRefusedException:
                 self.catalog_services.remove(catalog_prx)
 
-        print("\n[MAIN] No hay servicios de autenticación disponibles")
+        print(f"\n[MAIN] ID: {self.service_id} No hay servicios de MediaCatalog disponibles")
         raise IceFlix.TemporaryUnavailable
-    
-    def addCatalog(self, catalog_prx, current=None):
+
+
+    def updateDB(self, values, service_id, current=None):  # pylint: disable=invalid-name,unused-argument
+        """Receives the current main service database from a peer."""
+
+        print(f"[MAIN] ID: {self.service_id}. Recibido VolatileServices de {service_id}.")
+
+        if service_id == self.service_id:
+            return
+
+        if not self.auth_services or not self.catalog_services:
+            raise IceFlix.UnknownService
+
+        if not self.actualizado:
+            self.auth_services = values.get_authenticators()
+            self.catalog_services = values.get_catalogs()
+            print(f"[MAIN] ID: {self.service_id}. Se han actualizado los servicios de" +
+                  f"autenticación y catálogo")
+            self.actualizado = True
+
+    def addCatalog(self, catalog_prx, current=None): # pylint: disable=invalid-name,unused-argument
+        """ Añade un MediaCatalog activo a su lista """
+
         self.catalog_services.append(catalog_prx)
-                
+
 
     def register(self, service, current=None): # pylint: disable=unused-argument
         ''' Permite registrarse a determinados servicios '''
@@ -90,30 +120,7 @@ class MainI(IceFlix.Main): # pylint: disable=inherit-non-class
     def share_data_with(self, service):
         """Share the current database with an incoming service."""
         service.updateDB(self.get_volatile_services, self.service_id)
-        
 
-    def updateDB(
-        self, values, service_id, current=None
-    ):  # pylint: disable=invalid-name,unused-argument
-        """Receives the current main service database from a peer."""
-        print(f"Receiving remote data base from {service_id} to {self.service_id}")
-        
-        if service_id == self.service_id:
-            print("[MAIN] No se puede actualizar la base de datos con la misma instancia")
-            return 
-
-        # TODO: COMPROBAR QUE ES TIPO MAIN(?)
-        
-        if not self.auth_services or not self.catalog_services:
-            raise IceFlix.UnknownService
-        
-        if not self.actualizado:
-            self.auth_services = values.get_authenticators()
-            self.catalog_services = values.get_catalogs()
-            print("[MAIN] Se han actualizado los servicios de autenticación y catálogo")
-            self.actualizado = True
-        
-        
 
 class MainServer(Ice.Application):
     """Servidor del servicio principal"""
@@ -131,13 +138,13 @@ class MainServer(Ice.Application):
         """Configure the announcements sender and listener."""
 
         communicator = self.communicator()
-        topic_manager = IceStorm.TopicManagerPrx.checkedCast(
+        topic_manager = TopicManagerPrx.checkedCast(
             communicator.propertyToProxy("IceStorm.TopicManager")
         )
 
         try:
             topic = topic_manager.create(ANNOUNCEMENT_TOPIC)
-        except IceStorm.TopicExists:
+        except TopicExists:
             topic = topic_manager.retrieve(ANNOUNCEMENT_TOPIC)
 
         self.announcer = ServiceAnnouncementsSender(
@@ -154,13 +161,13 @@ class MainServer(Ice.Application):
         topic.subscribeAndGetPublisher({}, subscriber_prx)
 
 
-    def run(self, argv):
+    def run(self, args):
         ''' Implementación del servidor principal '''
 
         broker = self.communicator()
 
         properties = broker.getProperties()
-        self.servant._token_ = properties.getProperty("AdminToken")
+        self.servant._token_ = properties.getProperty("AdminToken") #pylint: disable=protected-access
 
         self.adapter = broker.createObjectAdapterWithEndpoints('MainAdapter', 'tcp')
         self.adapter.add(self.servant, broker.stringToIdentity("Main"))
