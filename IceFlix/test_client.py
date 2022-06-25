3e3c49219501eb5da4dd706cddf39b681ce5be93
Sergio Garrido <58768971+sergiogm8@users.noreply.github.com>
2022-06-25 13:43:52 +0200

    Test cliente authenticator

--- IceFlix/test_client.py
+++ IceFlix/test_client.py
@@ -11,6 +11,7 @@ import Ice
 import iceflixrtsp
 from MediaUploader import MediaUploaderI
 import IceStorm
+import secrets
 from user_revocations import RevocationsListener, RevocationsSender
 from constants import REVOCATIONS_TOPIC  #pylint: disable=no-name-in-module
 
@@ -34,8 +35,72 @@ class Cliente(Ice.Application): #pylint: disable=too-many-instance-attributes,to
         print(f"Conectado a authenticator en proxy {auth_prx}")
         
         # Conexión hecha, se pueden probar las cosas
-        print("Prueba isAuthorized(Manoloooo)")
-        print(auth_connection.isAuthorized("Manolooooo"))
+        token = secrets.token_urlsafe(40)
+        print("PRUEBAS TOKEN NO VALIDO, expected False y UNAUTHORIZED")
+        print("isAuthorized()=", auth_connection.isAuthorized(token))
+        try:
+            print("whois()=", auth_connection.whois(token), ", FAIL")
+        except IceFlix.Unauthorized as e:
+            print("whois()=", e, ", EXITO")
+        
+        print("\nPRUEBA USUARIO NO EXISTE, expected UNAUTHORIZED")
+        user = "sergio"
+        password = "password"
+        hash = hashlib.sha256(password.encode()).hexdigest()
+        try:
+            print("refreshAuthorization()= ", auth_connection.refreshAuthorization(user, hash), ", FAIL")
+        except IceFlix.Unauthorized as e:
+            print("refreshAuthorization()= ", e, ", EXITO")
+            
+        print("\nPRUEBA REFRESHAUTH USUARIO EXISTE, expected TOKEN")
+        user2 = "user"
+        pass2 = "password"
+        hash = hashlib.sha256(pass2.encode()).hexdigest()
+        try:
+            token_valido = auth_connection.refreshAuthorization(user2, hash)
+            print("refreshAuthorization()= ", token_valido, "EXITO")
+        except IceFlix.Unauthorized as e:
+            print("refreshAuthorization()= ", e, ", FAIL")
+        
+        print("\nPRUEBA AÑADIR/BORRAR USUARIOS SIN TOKEN ADMIN, expected UNAUTHORIZED or TEMPUNAVAILABLE ")
+        admin_token = "token"
+        try:
+            print("addUser()=", auth_connection.addUser(user, hash, admin_token), "FAIL")
+        except IceFlix.Unauthorized as e:
+            print("addUser()=", e, ", EXITO")
+        except IceFlix.TemporaryUnavailable as e:
+            print("addUser()=", e, ", FAIL")
+            
+        try:
+            print("removeUser()=", auth_connection.removeUser(user, admin_token), "FAIL")
+        except IceFlix.Unauthorized as e:
+            print("removeUser()=", e, ", EXITO")
+        except IceFlix.TemporaryUnavailable as e:
+            print("removeUser()=", e, ", FAIL")
+        
+        print("\nPRUEBA CON TOKEN VALIDO, expected TRUE y USUARIO")
+        print("isAuthorized()=", auth_connection.isAuthorized(token_valido))
+        try:
+            print("whois()=", auth_connection.whois(token_valido), ", EXITO")
+        except IceFlix.Unauthorized as e:
+            print("whois()=", e, ", FRACASO")
+            
+        print("\nPRUEBA AÑADIR/BORRAR USUARIOS CON TOKEN ADMIN, expected None (EXITO) ")
+        admin_token = "admin"
+        try:
+            print("addUser()=", auth_connection.addUser(user, hash, admin_token), "EXITO")
+        except IceFlix.Unauthorized as e:
+            print("addUser()=", e, ", FAIL")
+        except IceFlix.TemporaryUnavailable as e:
+            print("addUser()=", e, ", FAIL")
+
+        try:
+            print("removeUser()=", auth_connection.removeUser(user, admin_token), "EXITO")
+        except IceFlix.Unauthorized as e:
+            print("removeUser()=", e, ", FAIL")
+        except IceFlix.TemporaryUnavailable as e:
+            print("removeUser()=", e, ", FAIL")
+            
 
 if __name__ == "__main__":
     sys.exit(Cliente().main(sys.argv))
